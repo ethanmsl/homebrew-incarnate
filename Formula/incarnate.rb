bf6df7271ab58217fb6606c3b1bbb59b9f60614a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-23 09:42:37 -0700

    v0.1.0 & aarch64 namign convention for binary

--- Formula/incarnate.rb
+++ Formula/incarnate.rb
@@ -3,9 +3,9 @@
 class Incarnate < Formula
   desc "Interactive Skeleton (repo) Enflesher"
   homepage "https://github.com/ethanmsl/incarnate"
-  url "https://github.com/ethanmsl/incarnate/releases/download/alpha/incarnate-mac_m1.tar.gz"
-  sha256 "9299a513fc0661034e0c417790cc0e926d6a1aad525053c638bcc0d7deee5c63"
-  version "0.1.0"
+  url "https://github.com/ethanmsl/incarnate/releases/download/v0.1.1/incarnate-apple_aarch64.tar.gz"
+  sha256 "ae124e3a3d1353b235abda4c4e18023f1f02e3e2c540368b9ea1f0369eaeb1c2"
+  version "0.1.1"
 
   def install
     bin.install "incarnate"
