500b633b6cba0d0b997d69668b89823fa5754b59
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-23 09:49:55 -0700

    fix(minor): updated version number in binary

--- Formula/incarnate.rb
+++ Formula/incarnate.rb
@@ -4,7 +4,7 @@ class Incarnate < Formula
   desc "Interactive Skeleton (repo) Enflesher"
   homepage "https://github.com/ethanmsl/incarnate"
   url "https://github.com/ethanmsl/incarnate/releases/download/v0.1.1/incarnate-apple_aarch64.tar.gz"
-  sha256 "ae124e3a3d1353b235abda4c4e18023f1f02e3e2c540368b9ea1f0369eaeb1c2"
+  sha256 "fe62a77610de53918b0a103e8d312f6f04d2688eb8a956c93317ccaef135dea3"
   version "0.1.1"
 
   def install
