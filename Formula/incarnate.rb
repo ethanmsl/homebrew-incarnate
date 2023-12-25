5d00a1e6efe16f682b11ac85d8a0a79e0e4f1047
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-25 11:33:36 +0100

    fix(attempt): attempt to interface update with HomeBrew system

--- Formula/incarnate.rb
+++ Formula/incarnate.rb
@@ -1,9 +1,11 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
 class Incarnate < Formula
   desc "Interactive Skeleton (repo) Enflesher"
   homepage "https://github.com/ethanmsl/incarnate"
-  url "https://github.com/ethanmsl/incarnate/releases/download/v0.2.1/incarnate"
-  version "0.2.2"
+  url "https://github.com/ethanmsl/incarnate/releases/download/0.2.2/incarnate-aarch64-apple-darwin.tar.gz"
   sha256 "bf83d8d8efdb84c0fc2d82fc261c7213ede877ebbcffd5356977e5ef5d69d63a"
+  version "0.2.2"
 
   def install
     bin.install "incarnate"
