788e7d901c0762f2d89c3d60e286c94a227ee63c
ethanmsl <33399972+ethanmsl@users.noreply.github.com>
2023-12-25 01:14:40 +0100

    fix: Update incarnate.rb
    responding to errors from `brew bump-formula-pr --url... --version... --sha256...`

--- Formula/incarnate.rb
+++ Formula/incarnate.rb
@@ -1,11 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Incarnate < Formula
   desc "Interactive Skeleton (repo) Enflesher"
   homepage "https://github.com/ethanmsl/incarnate"
   url "https://github.com/ethanmsl/incarnate/releases/download/v0.2.1/incarnate"
-  sha256 "9b37e20b2255764bebc0f02c8847445ed51510da02f4df9b34d08bf9199b631d"
   version "0.1.1"
+  sha256 "9b37e20b2255764bebc0f02c8847445ed51510da02f4df9b34d08bf9199b631d"
 
   def install
     bin.install "incarnate"
