a5479908a75740a7f4ee7f68189bdda6208f3d0b
ethanmsl <33399972+ethanmsl@users.noreply.github.com>
2023-12-25 02:36:04 +0100

    Update incarnate.rb

--- Formula/incarnate.rb
+++ Formula/incarnate.rb
@@ -2,8 +2,8 @@ class Incarnate < Formula
   desc "Interactive Skeleton (repo) Enflesher"
   homepage "https://github.com/ethanmsl/incarnate"
   url "https://github.com/ethanmsl/incarnate/releases/download/v0.2.1/incarnate"
-  version "0.2.1"
-  sha256 "64d6e78594721e8abc344895598f1d9f18d9ca89bfe10aa4bf0382d69afeb536"
+  version "0.2.2"
+  sha256 "bf83d8d8efdb84c0fc2d82fc261c7213ede877ebbcffd5356977e5ef5d69d63a"
 
   def install
     bin.install "incarnate"
