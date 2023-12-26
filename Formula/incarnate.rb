08a36efcb783b1f1ce8ab7c6324156bdde94ddfd
ethanmsl <33399972+ethanmsl@users.noreply.github.com>
2023-12-26 10:05:15 -0800

    Update incarnate.rb

--- Formula/incarnate.rb
+++ Formula/incarnate.rb
@@ -1,13 +1,14 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Incarnate < Formula
   desc "Interactive Skeleton (repo) Enflesher"
   homepage "https://github.com/ethanmsl/incarnate"
   url "https://github.com/ethanmsl/incarnate/releases/download/0.2.3/incarnate-aarch64-apple-darwin.tar.gz"
   sha256 "6d3c184d73cf377f5a400e4d98a44c8925be1070fc19c6f9e4b7bbbadd1fa2bc"
   version "0.2.3"
+  license ""
 
   def install
     bin.install "incarnate"
+    #system "./configure", *std_configure_args, "--disable-silent-rules"
   end
 end
+
