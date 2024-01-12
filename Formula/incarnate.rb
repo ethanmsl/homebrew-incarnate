52cf7a44abff1db179fa26f0dee95107eb49946e
ethanmsl <33399972+ethanmsl@users.noreply.github.com>
2024-01-12 07:15:33 +0100

    Update incarnate.rb

--- Formula/incarnate.rb
+++ Formula/incarnate.rb
@@ -1,9 +1,9 @@
 class Incarnate < Formula
   desc "Interactive Skeleton (repo) Enflesher"
   homepage "https://github.com/ethanmsl/incarnate"
-  url "https://github.com/ethanmsl/incarnate/releases/download/0.2.9/incarnate-aarch64-apple-darwin.tar.gz"
-  sha256 "1af95555815ca3a1e9df84b273c944b78e5451ae3286a229719eb07f0515fd86"
-  version "0.2.9"
+  url "https://github.com/ethanmsl/incarnate/releases/download/0.2.10/incarnate-aarch64-apple-darwin.tar.gz"
+  sha256 "882ed1aa7733b062d7c2565a7ca47b2f0462a72ec369e9d3376800fc9b7c7213"
+  version "0.2.10"
   license ""
 
   def install
